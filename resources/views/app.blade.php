d0088c3c524456bd6c00550571060a0dddd2d15e
Michael Bock <michael@michaelrbock.com>
2015-04-28 13:17:46 -0700

    test

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -41,13 +41,13 @@
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				<ul class="nav navbar-nav navbar-right">
 					<li>
-						<a href="#about">About</a>
+						<a href="/">About</a>
 					</li>
 					<li>
-						<a href="#services">Services</a>
+						<a href="/">Login</a>
 					</li>
 					<li>
-						<a href="#contact">Contact</a>
+						<a href="/signup">Signup</a>
 					</li>
 				</ul>
 			</div>
