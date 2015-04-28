0ff628107245518bade2312d602073004f8fa96d
Michael Bock <michael@michaelrbock.com>
2015-04-27 21:07:17 -0700

    try more things

--- config/database.php
+++ config/database.php
@@ -2,10 +2,10 @@
 
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-$host = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$database = substr($url["path"], 1);
+$host = '';//$url["host"];
+$username = '';//$url["user"];
+$password = '';//$url["pass"];
+$database = '';//substr($url["path"], 1);
 
 return [
 
@@ -61,10 +61,10 @@ return [
 
 		'mysql' => [
 			'driver'    => 'mysql',
-			'host'      => env('DB_HOST', 'localhost'),
-			'database'  => env('DB_DATABASE', 'forge'),
-			'username'  => env('DB_USERNAME', 'forge'),
-			'password'  => env('DB_PASSWORD', ''),
+			'host'      => $host, //env('DB_HOST', 'localhost'),
+			'database'  => $database, //env('DB_DATABASE', 'forge'),
+			'username'  => $username, //env('DB_USERNAME', 'forge'),
+			'password'  => $password, //env('DB_PASSWORD', ''),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
