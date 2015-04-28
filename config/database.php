a41dddd0161968b78e327e6711e88dd2c5b64c69
Michael Bock <michael@michaelrbock.com>
2015-04-27 20:49:41 -0700

    try something

--- config/database.php
+++ config/database.php
@@ -61,10 +61,10 @@ return [
 
 		'mysql' => [
 			'driver'    => 'mysql',
-			'host'      => $host,
-			'database'  => $database,
-			'username'  => $username,
-			'password'  => $password,
+			'host'      => env('DB_HOST', 'localhost'),
+			'database'  => env('DB_DATABASE', 'forge'),
+			'username'  => env('DB_USERNAME', 'forge'),
+			'password'  => env('DB_PASSWORD', ''),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
